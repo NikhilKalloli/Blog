96fa1cde783c63123a087d8ea422ec250df5eeaf
Nikhil <Nikhil@nikhilkalloli0097.com>
2023-12-05 18:41:49 +0530

    Configured api endpoints

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-const API_URL = "http://localhost:4000";
+const API_URL = "https://my-api-tjit.onrender.com";
+
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
@@ -59,7 +61,7 @@ app.post("/api/posts/:id", async (req, res) => {
       `${API_URL}/posts/${req.params.id}`,
       req.body
     );
-    console.log(response.data);
+    // console.log(response.data);
     res.redirect("/");
   } catch (error) {
     res.status(500).json({ message: "Error updating post" });
@@ -77,5 +79,5 @@ app.get("/api/posts/delete/:id", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Backend server is running on http://localhost:${port}`);
+  console.log(`Backend server is running on PORT: ${port}`);
 });
